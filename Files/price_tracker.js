7fa145ee497cd8f3b47ec93d0e11eeb23e3e830d
Martin Ashby <martin@ashbysoft.com>
2015-03-18 20:32:00 +0000

    Added notification setting saving in local data.  Also added repeating poll for price data in notifications, and checking of the results.
     Still need to add the actual notification panel (with a dismiss option?)

--- Files/price_tracker.js
+++ Files/price_tracker.js
@@ -7,6 +7,7 @@ function price_tracker() {
     const notification_storage_key_prefix = "notification_item_id_";
     const price_url_root = "https://api.guildwars2.com/v2/commerce/prices?ids=";
 
+    // Functions for storage
     function check_storage_version() {
         const storage_version_key = "tracked_items_version";
         const storage_version = "3";
@@ -33,6 +34,8 @@ function price_tracker() {
     }
 
     var tracked_items = get_object_array_from_storage(tracked_items_key);
+
+    var notified_items = [];
 
     // Format prices using the coin icons
     function format_price(price){
@@ -62,17 +65,20 @@ function price_tracker() {
         var ngold = Math.round((price / 10000));
         return {ngold:ngold, nsilver:nsilver, ncopper: ncopper};
     }
+
     function unsplit_price(gold, silver, copper) {
         return (10000 * parseInt(gold)) + (100 * parseInt(silver)) + parseInt(copper);
     }
 
-    // A tab at the side of the screen to reveal the price tracker
     var $reveal_button = $('<div id="reveal_tracked_items">Prices</div>');
     var $item_list_and_controls = $('<div id="tracked_items_list"></div>');
     var $add_button = $('<button class="tracker_button">add</button>');
     var $close_button = $('<button class="tracker_button">close</button>');
     var $item_browser = item_browser(browser_callback);
     var $item_list = item_list(list_callback, list_display_html_function);
+    var $notification_panel = $('<div id="notification_panel"></div>');
+    var $notification_item_list = item_list(notified_list_callback, list_display_html_function);
+    var $notify_close_button = $('<button class="tracker_button">Close</button>');
     var $body = $('body');
 
     $item_list_and_controls.append($add_button);
@@ -81,8 +87,13 @@ function price_tracker() {
     $item_list_and_controls.hide();
     $item_browser.hide();
 
+    $notification_panel.append($notify_close_button);
+    $notification_panel.append($notification_item_list);
+    $notification_panel.hide();
+
     $body.append($item_list_and_controls);
     $body.append($item_browser);
+    $body.append($notification_panel);
 
     $reveal_button.hover(function(){
         $reveal_button.animate({left: "+=70px"});
@@ -94,7 +105,6 @@ function price_tracker() {
         $item_list_and_controls.show(150);
     });
 
-    // Buttons for adding/removing from the list
     $add_button.click(function(){
         $item_browser.show_browser();
     });
@@ -103,13 +113,11 @@ function price_tracker() {
         $item_list_and_controls.hide(150);
     });
 
-    function rebuild_item_list(){
-        $item_list.display_all_item_ids(tracked_items);
-    }
-    // Call it once it's defined.
-    rebuild_item_list();
+    $notify_close_button.click(function() {
+        notified_items = [];
+        $notification_panel.hide(150);
+    });
 
-    // Add an item list for the currently tracked items
     function list_callback($li, item_id) {
         // Show the buttons panel
         var $buttons_list = $li.$buttons_list;
@@ -120,7 +128,6 @@ function price_tracker() {
         }
     }
 
-    // Display the name AND current cost of the item
     function list_display_html_function($li, item_data) {
         var name = item_data.name;
         var item_id = item_data.id;
@@ -178,7 +185,6 @@ function price_tracker() {
     }
 
     function show_notification_settings($li, item_data) {
-        // Check for existing notifications for this item id
         var $notification_div = $('<div class="notification_settings">Notify me when the ' +
         '<select id="buy_sell_select" class="notification_select">' +
         '<option value="buys">buy</option>' +
@@ -254,39 +260,99 @@ function price_tracker() {
         $notification_div.offset({top: li_offset.top + li_height, left: original_offset.left});
     }
 
-    // Start polling the GW2 API to see when the price changes
-    (function poll() {
-        setTimeout(function() {
-            //retrieve keys from storage
-            for (var i=0; i<localStorage.length; i++) {
-                const key = localStorage.key(i);
-                if (key.indexOf(notification_storage_key_prefix) === 0) {
-                    var item_id = key.substr(notification_storage_key_prefix.length);
+    function notified_list_callback($li, item_id) {
+        localStorage.removeItem(notification_storage_key_prefix + item_id);
+        var ix = notified_items.indexOf(parseInt(item_id));
+        if (ix > -1) {
+            notified_items.splice(ix, 1);
+        }
+        if (notified_items.length) {
+            $notification_item_list.display_all_item_ids(notified_items);
+        } else {
+            $notification_panel.hide(150);
+        }
+    }
 
-                    function success(price_data) {
-                        price_data = price_data[0];
-                        var key = notification_storage_key_prefix + price_data.id;
-                        var notification_setting = get_object_array_from_storage(key);
-                        var price = price_data[notification_setting.buy_sell].unit_price;
-                        var show = false;
-                        if (notification_setting.operator === "gt") {
-                            show = price > notification_setting.price;
-                        } else if (notification_setting.operator === "lt") {
-                            show = price < notification_setting.price;
-                        }
-                        if (show) {
-                            console.log("would have shown a notification...")
-                        }
-                    }
+    function notification_list_item_html($li, item_data) {
+        var notification_settings = get_object_array_from_storage(notification_storage_key_prefix + item_data.id);
+        $li.append($('<span>' + item_data.name + ' has reached target ' + notification_settings.buy_sell + ' price of ' +
+            format_price(notification_settings.price) + ' </span>'));
+    }
 
-                    $.ajax({
-                        url: price_url_root + item_id,
-                        success: success,
-                        complete: poll
-                    });
+    function show_notification(item_id, notification_setting) {
+        // Check if we're already showing this item
+        if (notified_items.indexOf(item_id) > -1) {
+            return;
+        }
+
+        // Add to the list of notified items
+        notified_items.push(item_id);
+
+        // Show the notification list if required
+        if (notified_items.length > 0) {
+            if (!$notification_panel.is(':visible')) {
+                $notification_panel.show(150);
+            }
+
+            // Display the list of items we're notifying for
+            $notification_item_list.display_all_item_ids(notified_items);
+        }
+    }
+
+    function handle_price_data(price_data_list) {
+        for (var i=0; i<price_data_list.length; i++) {
+            // Get the notification for this price data.
+            var price_data = price_data_list[i];
+            var item_id = price_data.id;
+
+            // Got to check if it still exists.
+            var notification_setting = get_object_array_from_storage(notification_storage_key_prefix + item_id);
+            if (typeof(notification_setting) !== 'undefined'
+                && notification_setting.hasOwnProperty('buy_sell')) {
+                // Now notify if required.
+                var buy_or_sell = notification_setting.buy_sell;
+                var price = price_data[buy_or_sell].unit_price;
+                var show = false;
+                if (notification_setting.operator === "gt") {
+                    show = price > notification_setting.price;
+                } else {
+                    show = price < notification_setting.price;
+                }
+                if (show) {
+                    show_notification(item_id, notification_setting);
                 }
             }
-        }, 3000)
+        }
+    }
+
+    // Self-calling functions go last
+    function rebuild_item_list(){
+        $item_list.display_all_item_ids(tracked_items);
+    }
+    rebuild_item_list();
+
+    // Start polling the GW2 API to see when the price changes
+    (function poll() {
+        setTimeout(function (){
+            // Get IDs to poll for from storage
+            var ids = "";
+            for (var i=0; i<localStorage.length; i++) {
+                var key = localStorage.key(i);
+                if (key.indexOf(notification_storage_key_prefix) === 0) {
+                    if (i > 0) {
+                        ids += ",";
+                    }
+                    ids += key.substr(notification_storage_key_prefix.length);
+                }
+            }
+
+            $.ajax({
+                url: price_url_root + ids,
+                timeout: 30000,
+                success: handle_price_data,
+                complete: poll
+            });
+        }, 3000);
     })();
 
     return $reveal_button;
