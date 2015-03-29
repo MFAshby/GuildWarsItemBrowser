b2e0e12394f8f28549427ba156d37227591f0f0e
drakett <daniel@drakett.co.uk>
2015-03-29 21:31:16 +0100

    fixed rounding of item prices

--- GuildWarsItemBrowser/Files/draggable.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $(document).mousedown(function () {
-    overwolf.windows.getCurrentWindow(function(result) {
-      console.log("mousedown called");
-      overwolf.windows.dragMove(result.window.id);
-    });
-  });
-});
