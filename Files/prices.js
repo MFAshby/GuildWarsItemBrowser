49d32a7ab2e6260923e52de66fc537976510c176
glitterbat <jessica@glitterbat.net>
2015-03-29 12:51:48 +0100

    Price text colour change.

--- Files/prices.js
+++ Files/prices.js
@@ -19,15 +19,15 @@ var prices = {
     var price_obj = this.split_price(price);
     var $price_description = $('<span></span>');
     if (price_obj.ngold > 0) {
-      $price_description.append("" + price_obj.ngold);
+      $price_description.append('<span style="color:#fdc84e">' + price_obj.ngold + '</span>');
       $price_description.append($('<img src="images/Gold_coin.png"/>'));
     }
     if (price_obj.nsilver > 0) {
-      $price_description.append("" + price_obj.nsilver);
+      $price_description.append('<span style="color:#cbcbcb">' + price_obj.nsilver + '</span>');
       $price_description.append($('<img src="images/Silver_coin.png"/>'));
     }
     if (price_obj.ncopper > 0) {
-      $price_description.append("" + price_obj.ncopper);
+      $price_description.append('<span style="color:#d47f46">' + price_obj.ncopper + '</span>');
       $price_description.append($('<img src="images/Copper_coin.png"/>'));
     }
     if ($price_description.text() === "") {
