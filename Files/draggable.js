b28603afa3008ac7d2890221be571cfe7d3cd0fc
Martin Ashby <martin@ashbysoft.com>
2015-04-01 23:16:49 +0100

    Added resize grips to all windows in the bottom right.

--- Files/draggable.js
+++ Files/draggable.js
@@ -5,4 +5,15 @@ $(document).ready(function () {
       overwolf.windows.dragMove(result.window.id);
     });
   });
+
+  $resize_grip = $('.resize_grip');
+  if ($resize_grip) {
+    $resize_grip.mousedown(function () {
+      overwolf.windows.getCurrentWindow(function(result){
+        if (result.status=="success"){
+          overwolf.windows.dragResize(result.window.id, 'BottomRight');
+        }
+      });
+    });
+  }
 });
