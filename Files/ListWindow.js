39efd00b30a3bddbd106bf61b62d1d6ea941fd73
Martin Ashby <martin@ashbysoft.com>
2015-04-01 23:37:54 +0100

    Remove the pointless widget window

--- Files/ListWindow.js
+++ Files/ListWindow.js
@@ -134,4 +134,46 @@ $(document).ready(function (){
 
   // Call this on page load
   rebuild_item_list();
+
+  // This window is always around, so it's reponsible for checking prices and popping up if anything hits a target.
+  function handle_price(price_data) {
+    try
+    {
+      var tracked_item = item_tracker.get_tracked_item(price_data.id);
+      if (typeof(tracked_item) === 'undefined') {
+        return false;
+      }
+
+      var notification_setting = tracked_item.notification_setting;
+      if (typeof(notification_setting) === 'undefined'
+       || !notification_setting.hasOwnProperty("buy_sell")) {
+        return false;
+      }
+
+      var current_price = price_data[notification_setting.buy_sell].unit_price;
+      var target_price = notification_setting.price;
+
+      if ((notification_setting.operator === "gt" && current_price > target_price)
+          || (notification_setting.operator === "lt" && current_price < target_price)) {
+        notification_setting.notified = "notified";
+        item_tracker.add_tracked_item(price_data.id, notification_setting);
+        show_list_window();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function check_prices() {
+    var tracked_items = item_tracker.get_tracked_items();
+    var tracked_item_ids = Object.keys(tracked_items);
+
+    $.get(item_tracker.price_url_root + tracked_item_ids.join(","), function(prices_data) {
+      prices_data.forEach(function (price_data) {
+        handle_price(price_data);
+      });
+      setTimeout(check_prices, 15000);
+    });
+  }
+  setTimeout(check_prices, 5000);
 });
